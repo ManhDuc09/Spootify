fdc23e65590c3be9097d5fac93f580af61d49558
ManhDuc09 <duccute3@gmail.com>
2025-04-07 08:43:57 +0000

    music player

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,18 +1,24 @@
 import { Outlet } from "react-router-dom";
 import TopNavBar from "./components/TopNavBar";
 import SideBar from "./components/SideBar";
+import MusicPlayer from "../components/MusicPlayer";
 
 const MainLayout = () => {
     return (
         <>
-            <TopNavBar />
-            <div className="flex">
+            <div className="flex h-screen w-screen">
+                <TopNavBar />
                 <SideBar />
-                <main className="flex-1 p-4 ml-64 mt-16 overflow-y-auto">
+
+                <main className="flex-1 p-16 ml-64 mt-16 overflow-y-auto">
                     <Outlet />
-                    jdioadasjd
                 </main>
             </div>
+
+            <div className="fixed bottom-0 left-76 right-0 z-50">
+                  <MusicPlayer />
+            </div>
+
         </>
     );
 };
