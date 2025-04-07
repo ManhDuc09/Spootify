6557ffd218ede84bababc3f4af5db3683f29e264
ManhDuc09 <duccute3@gmail.com>
2025-04-07 15:50:30 +0000

    music card skeleton

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,81 @@
 
 
+import MusicCard from '../components/MusicCard';
 import MusicPlayer from '../components/MusicPlayer'
 function Home() {
 
 
     return (
         <>
-            this is home page
-      
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">Trending Music</h1>
+
+            <div className="flex gap-4 overflow-x-auto pb-4">
+                <MusicCard
+                    title="Blinding Lights"
+                    artist="The Weeknd"
+                    image="https://link-to-album-cover.jpg"
+                />
+                <MusicCard
+                    title="Save Your Tears"
+                    artist="The Weeknd"
+                    image="https://link-to-another-cover.jpg"
+                />
+                <MusicCard
+                    title="In Your Eyes"
+                    artist="The Weeknd"
+                    image="https://yet-another-image.jpg"
+                />
+          
+            </div>
+
+        </div>
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">Trending Music</h1>
+
+            <div className="flex gap-4 overflow-x-auto pb-4">
+                <MusicCard
+                    title="Blinding Lights"
+                    artist="The Weeknd"
+                    image="https://link-to-album-cover.jpg"
+                />
+                <MusicCard
+                    title="Save Your Tears"
+                    artist="The Weeknd"
+                    image="https://link-to-another-cover.jpg"
+                />
+                <MusicCard
+                    title="In Your Eyes"
+                    artist="The Weeknd"
+                    image="https://yet-another-image.jpg"
+                />
+          
+            </div>
+
+        </div>
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">Trending Music</h1>
+
+            <div className="flex gap-4 overflow-x-auto pb-4">
+                <MusicCard
+                    title="Blinding Lights"
+                    artist="The Weeknd"
+                    image="https://link-to-album-cover.jpg"
+                />
+                <MusicCard
+                    title="Save Your Tears"
+                    artist="The Weeknd"
+                    image="https://link-to-another-cover.jpg"
+                />
+                <MusicCard
+                    title="In Your Eyes"
+                    artist="The Weeknd"
+                    image="https://yet-another-image.jpg"
+                />
+          
+            </div>
+
+        </div>
         </>
         
     )
