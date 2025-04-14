0472b205b95c215bc052c28cef6d46284c79edb8
ManhDuc09 <duccute3@gmail.com>
2025-04-14 15:43:17 +0000

    as

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -16,7 +16,20 @@ const MainLayout = () => {
             </div>
 
             <div className="fixed bottom-0 left-76 right-0 z-50">
-                  <MusicPlayer />
+            <MusicPlayer
+                track={{
+                    title: "A Sky Ful213123l of Stars",
+                    artist: "Coldplay",
+                    album: "Ghost Stories",
+                    coverUrl: "https://tailwindcss.com/img/card-top.jpg",
+                    duration: 260,
+                    currentTime: 40
+                }}
+                album={{
+                    title: "Ghost Stories",
+                    coverUrl: "https://tailwindcss.com/img/card-top.jpg"
+                }}
+            />
             </div>
 
         </>
